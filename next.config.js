8cac4753646814ec6e456a4331eed2342067963c
Jaime Aza <jaime.aza@decimetrix.com>
2024-09-28 11:44:49 -0500

    Add loader and products

--- next.config.js
+++ next.config.js
@@ -7,6 +7,17 @@ const nextConfig = {
     includePaths: [path.join(__dirname, "src/sass")],
     prependData: `@import "main.sass"`,
   },
+  images: {
+    domains: ["cdn.shopify.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.shopify.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
--- src/components/home/MainProductos/index.tsx
+++ src/components/home/MainProductos/index.tsx
@@ -1,4 +1,12 @@
-const getProducts = async () => {
+import Image from "next/image";
+import styles from "./MainProducts.module.sass";
+import type { Product } from "./Product.model";
+
+type ShopifyApiResponse = {
+  products: Product[];
+};
+
+const getProducts = async (): Promise<Product[]> => {
   const response = await fetch(
     `${process.env.SHOPIFY_HOST_NAME}/admin/api/2024-07/products.json`,
     {
@@ -8,16 +16,33 @@ const getProducts = async () => {
     }
   );
 
-  return response.json();
+  const data: ShopifyApiResponse = await response.json();
+  return data.products;
 };
 
 export const MainProducts = async () => {
-  const { products } = await getProducts();
+  const products = await getProducts();
 
   return (
-    <section>
-      <h2>MainProducts</h2>
-      <p>Products: {products.length}</p>
+    <section className={styles.MainProducts}>
+      <h3>✨ New products released!</h3>
+      <div className={styles.MainProducts__grid}>
+        {products &&
+          products?.map((product) => {
+            const imageSrc = product.images[0].src;
+            return (
+              <article key={product.id}>
+                <p>{product.title}</p>
+                <Image
+                  src={imageSrc}
+                  fill
+                  alt={product.title}
+                  loading="eager"
+                />
+              </article>
+            );
+          })}
+      </div>
     </section>
   );
 };
