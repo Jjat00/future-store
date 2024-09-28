f9c3c9b265638598be5ea59ae980d6988f51d944
Jaime Aza <jaime.aza@decimetrix.com>
2024-09-28 11:20:56 -0500

    Add var env and get products from shopify store

--- src/components/home/MainProductos/index.tsx
+++ src/components/home/MainProductos/index.tsx
@@ -1,7 +1,23 @@
-export const MainProducts = () => {
+const getProducts = async () => {
+  const response = await fetch(
+    `${process.env.SHOPIFY_HOST_NAME}/admin/api/2024-07/products.json`,
+    {
+      headers: {
+        "X-Shopify-Access-Token": process.env.SHOPIFY_API_KEY || "",
+      },
+    }
+  );
+
+  return response.json();
+};
+
+export const MainProducts = async () => {
+  const { products } = await getProducts();
+
   return (
     <section>
       <h2>MainProducts</h2>
+      <p>Products: {products.length}</p>
     </section>
   );
 };
