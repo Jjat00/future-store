e988f7b8aa6e405f59e245a3e009575a700fa698
Jaime Aza <jaime.aza@decimetrix.com>
2024-10-22 22:57:15 -0500

    Add SEO metadata

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,11 @@
 import { MainProducts } from "app/components/home/MainProductos";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Future world",
+  description: "Empowering Your Tomorrow, Today!",
+  keywords: ["future", "technology", "world", "ecommerce"],
+};
 
 export default function Home() {
   return (
--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -8,6 +8,30 @@ interface propsProduct {
   };
 }
 
+export async function generateMetadata({ searchParams }: propsProduct) {
+  const { id } = searchParams;
+
+  const products = await getProducts(id);
+
+  return {
+    title: products[0].title,
+    description: products[0].body_html,
+    keywords: products[0].tags,
+    openGraph: {
+      title: products[0].title,
+      description: products[0].body_html,
+      url: `https://futureworld.com/product/${products[0].handle}`,
+      images: [
+        {
+          url: products[0].image.src,
+          width: 800,
+          height: 600,
+        },
+      ],
+    },
+  };
+}
+
 export default async function ProductPage({ searchParams }: propsProduct) {
   const { id } = searchParams;
 
